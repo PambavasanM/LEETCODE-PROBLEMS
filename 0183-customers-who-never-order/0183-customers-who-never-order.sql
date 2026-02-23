d21c6d1b30695c25eb0d4fb535b62f3a7332dea2
Pambavasan M <144297025+PambavasanM@users.noreply.github.com>
2026-02-23 23:24:24 +0530

    Time: 513 ms (76.28%), Space: 0B (100%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.sql
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -1,2 +1,3 @@
-select name as Customers from customers 
-where id not in (select customerId from Orders);
+SELECT c.name as Customers
+FROM Customers c
+WHERE id NOT IN (SELECT customerId FROM Orders)
