578a637674ee1dc04ce6c3fa38f1e03597ad0f6d
Pambavasan M <144297025+PambavasanM@users.noreply.github.com>
2026-03-23 14:13:51 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
+++ 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
@@ -1,5 +1,4 @@
-SELECT * 
-FROM patients
-WHERE conditions LIKE 'DIAB1%' 
-   OR conditions LIKE '% DIAB1%' 
-   OR conditions LIKE '% DIAB1';
+SELECT patient_id, patient_name, conditions
+FROM Patients
+WHERE conditions LIKE 'DIAB1%'
+   OR conditions LIKE '% DIAB1%';
