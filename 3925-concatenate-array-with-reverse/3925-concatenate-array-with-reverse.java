636dd666f47914c139f51cad04d4376c383ae0c9
Pambavasan M <144297025+PambavasanM@users.noreply.github.com>
2026-05-22 09:35:55 +0530

    Time: 1 ms (99.99%), Space: 47.1 MB (95.96%) - LeetHub

---
+++ 3925-concatenate-array-with-reverse/3925-concatenate-array-with-reverse.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] concatWithReverse(int[] nums) {
+        int nums2[] = new int[nums.length];
+        for(int i = 0; i < nums.length; i++)
+        {
+            nums2[nums.length-i-1] = nums[i];
+        }
+        int arr[] = new int[nums.length+nums2.length];
+        System.arraycopy(nums, 0, arr, 0, nums.length);
+        System.arraycopy(nums2, 0, arr, nums.length, nums2.length);
+        return arr;
+    }
+}
